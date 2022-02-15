fab58e39913a6f3527de15c741e7fc5fcf4ee6fe
Aleksey Belonogov <aleshka59000@gmail.com>
2022-02-15 16:44:27 +0300

    el problemo

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print ("я люблю аниме")
+import numpy as np
+import pandas as pd
+# if __name__ == main:
+# print(1)
+if __name__ == '__main__':
+    data=pd.read_csv("dataSpell/dataset.csv")
+    print(data[1])
