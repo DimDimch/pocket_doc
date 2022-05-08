8b476a70a02a075ec3699381f7223b23c08ac59f
ybelonogov <aleshka59000@gmail.com>
2022-05-08 14:37:25 +0300

    clent-server done

---
+++ client/client/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace client
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbPort = new System.Windows.Forms.TextBox();
+            this.tbIP = new System.Windows.Forms.TextBox();
+            this.tbMessage = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbPort
+            // 
+            this.tbPort.Location = new System.Drawing.Point(518, 31);
+            this.tbPort.Name = "tbPort";
+            this.tbPort.Size = new System.Drawing.Size(100, 35);
+            this.tbPort.TabIndex = 1;
+            // 
+            // tbIP
+            // 
+            this.tbIP.Location = new System.Drawing.Point(165, 33);
+            this.tbIP.Name = "tbIP";
+            this.tbIP.Size = new System.Drawing.Size(290, 35);
+            this.tbIP.TabIndex = 2;
+            // 
+            // tbMessage
+            // 
+            this.tbMessage.Location = new System.Drawing.Point(126, 102);
+            this.tbMessage.Name = "tbMessage";
+            this.tbMessage.Size = new System.Drawing.Size(492, 35);
+            this.tbMessage.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 30;
+            this.listBox1.Items.AddRange(new object[] {
+            "Output"});
+            this.listBox1.Location = new System.Drawing.Point(25, 189);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(992, 364);
+            this.listBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 30);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "IP/Hostname";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(462, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 30);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Port";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 30);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Message";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(624, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 40);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Отправить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(769, 95);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(155, 40);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Принять";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(624, 26);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(300, 40);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Установить соединение";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(886, 559);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(131, 40);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Закрыть";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1029, 615);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.tbMessage);
+            this.Controls.Add(this.tbIP);
+            this.Controls.Add(this.tbPort);
+            this.Name = "Form1";
+            this.Text = "Client";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox tbPort;
+        private System.Windows.Forms.TextBox tbIP;
+        private System.Windows.Forms.TextBox tbMessage;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox port;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ client/client/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace client
+{
+    public partial class Form1 : Form
+    {
+        int port_num;
+        string message;
+        int byteCount;
+        NetworkStream stream;
+        byte[] sendData;
+        byte[] getData;
+        TcpClient client;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                message = tbMessage.Text;
+                byteCount = Encoding.UTF8.GetByteCount(message);
+                sendData = new byte[byteCount];
+                sendData = Encoding.UTF8.GetBytes(message);
+                stream = client.GetStream();
+                stream.Write(sendData, 0, sendData.Length);
+                listBox1.Items.Add("- " + message);
+            }
+            catch (System.NullReferenceException)
+            {
+                MessageBox.Show("Отправка не удалась");
+                listBox1.Items.Add("Отправка не удалась");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(tbPort.Text, out port_num))
+            {
+                MessageBox.Show("Неверный порт");
+            }
+            try
+            {
+                client = new TcpClient(tbIP.Text, port_num);
+                MessageBox.Show("Соединение установлено");
+            }
+            catch (System.Net.Sockets.SocketException)
+            {
+                MessageBox.Show("Соединение не установлено");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                getData = new byte[1024];
+                stream = client.GetStream();
+                int num_of_read = stream.Read(getData, 0, 1024);
+
+                listBox1.Items.Add("- " + Encoding.UTF8.GetString(getData, 0, num_of_read));
+            }
+            catch (System.NullReferenceException)
+            {
+                MessageBox.Show("Отправка не удалась");
+                listBox1.Items.Add("Отправка не удалась");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            stream.Close();
+            client.Close();
+            listBox1.Items.Add("Соединение закрыто");
+        }
+    }
+}
